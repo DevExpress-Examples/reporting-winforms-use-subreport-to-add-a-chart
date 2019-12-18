780f3a9577aff180c665872f2345d54fcde7458b
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2019-12-18 16:21:23 +0300

    auto sync for version 2019.2

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Reporting-Use-Subreport-To-Add-A-Chart
+<!-- default file list -->
+*Files to look at*:
+
+* [Form1.cs](./CS/Use-Subreport-To-Add-A-Chart/Form1.cs)
+<!-- default file list end -->
+# Use Subreports to Add a Chart
+
+This project performs the following tasks:
+
+1. Create the main report that lists products grouped by category.
+   - The report's [GroupHeader](https://docs.devexpress.com/XtraReports/DevExpress.XtraReports.UI.GroupHeaderBand) band displays the category name.
+     - The band's [GroupFields](https://docs.devexpress.com/XtraReports/DevExpress.XtraReports.UI.GroupHeaderBand.GroupFields) collection specifies the group field (the **CategoryID** field).
+   - The report's [Detail](https://docs.devexpress.com/XtraReports/DevExpress.XtraReports.UI.DetailBand) band lists products from the group field's category.
+   - The report's [GroupFooter](https://docs.devexpress.com/XtraReports/DevExpress.XtraReports.UI.GroupFooterBand) band contains an [XRSubreport](https://docs.devexpress.com/XtraReports/DevExpress.XtraReports.UI.XRSubreport) control instance.
+     - The **XRSubreport**'s [ReportSource](https://docs.devexpress.com/XtraReports/DevExpress.XtraReports.UI.SubreportBase.ReportSource) property references a chart report.
+     - The **XRSubreport**'s [ParameterBindings](https://docs.devexpress.com/XtraReports/DevExpress.XtraReports.UI.XRSubreport.ParameterBindings) collection binds the chart report's **srptCategory** parameter to the main report's group field (the **CategoryID** field).
+2. Create a chart report that visualizes a specific category's products and prices.
+   - The chart report uses an [XRChart](https://docs.devexpress.com/XtraReports/DevExpress.XtraReports.UI.XRChart) control instance.
+   - The chart's [Parameters](https://docs.devexpress.com/XtraReports/DevExpress.XtraReports.UI.XRChart.Parameters) collection specifies the **chartCategory** parameter that is bound to the report's **srptCategory** parameter.
+   - The chart series' [FilterString](https://docs.devexpress.com/CoreLibraries/DevExpress.XtraCharts.SeriesBase.FilterString) property uses the **ChartCategory** parameter value to filter data.
+
+Refer to the [Merge Reports: Use Data-Driven Page Sequence](https://docs.devexpress.com/XtraReports/400691) topic for more information on how to add a chart to a report in the Report Designer.
---
+++ VB/Reporting-Use-Subreport-To-Add-A-Chart/Form1.vb
@@ -0,0 +1,151 @@
+﻿Imports System
+Imports System.Drawing
+Imports System.Windows.Forms
+Imports System.Collections.Generic
+Imports DevExpress.XtraReports.UI
+Imports DevExpress.DataAccess.Sql
+Imports DevExpress.DataAccess.ConnectionParameters
+Imports DevExpress.XtraPrinting
+Imports DevExpress.XtraCharts
+Imports DevExpress.XtraReports.Parameters
+Imports System.IO
+
+Namespace CreateSubreportsInCode
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+            Dim mainReport As XtraReport = CreateMainReport()
+            Dim designTool As New ReportDesignTool(mainReport)
+            ' Comment the next line to load the generated report in Report Designer.
+            'designTool.ShowRibbonDesignerDialog();
+            Dim printTool As New ReportPrintTool(mainReport)
+            ' Comment the next line to load the generated report in Print Preview.
+            printTool.ShowRibbonPreviewDialog()
+            Application.Exit()
+        End Sub
+        Private Function CreateMainReport() As XtraReport
+            Dim report As New XtraReport() With { _
+                .Bands = { _
+                    New GroupHeaderBand() With { _
+                        .GroupFields = { New GroupField("CategoryID") }, _
+                        .Controls = { _
+                            New XRLabel() With { _
+                                .ExpressionBindings = { New ExpressionBinding("BeforePrint", "Text", "[CategoryName]") }, _
+                                .BoundsF = New RectangleF(0,0,300,25), _
+                                .Font = New Font(New FontFamily("Arial"),12,FontStyle.Bold) _
+                            }, _
+                            New XRLabel() With { _
+                                .ExpressionBindings = { New ExpressionBinding("BeforePrint", "Text", "[Description]") }, _
+                                .BoundsF = New RectangleF(0,50,300,25), _
+                                .Font = New Font(New FontFamily("Arial"),9) _
+                            }, _
+                            New XRPictureBox() With { _
+                                .ExpressionBindings = { New ExpressionBinding("BeforePrint", "ImageSource", "[Picture]") }, _
+                                .BoundsF = New RectangleF(500,0,150,50), _
+                                .Sizing = ImageSizeMode.ZoomImage _
+                            }, _
+                            New XRLabel() With { _
+                                .Text = "Product Name", _
+                                .BoundsF = New RectangleF(50,100,400,25), _
+                                .Font = New Font(New FontFamily("Arial"),9,FontStyle.Bold) _
+                            }, _
+                            New XRLabel() With { _
+                                .Text = "Qty Per Unit", _
+                                .BoundsF = New RectangleF(450,100,100,25), _
+                                .Font = New Font(New FontFamily("Arial"),9,FontStyle.Bold) _
+                            }, _
+                            New XRLabel() With { _
+                                .Text = "Unit Price", _
+                                .BoundsF = New RectangleF(550,100,100,25), _
+                                .TextAlignment = TextAlignment.TopRight, _
+                                .Font = New Font(New FontFamily("Arial"),9,FontStyle.Bold) _
+                            } _
+                        } _
+                    }, _
+                    New DetailBand() With { _
+                        .Controls = { _
+                            New XRLabel() With { _
+                                .ExpressionBindings = { New ExpressionBinding("BeforePrint", "Text", "[ProductName]") }, _
+                                .BoundsF = New RectangleF(50,0,400,25), _
+                                .Font = New Font(New FontFamily("Arial"),9) _
+                            }, _
+                            New XRLabel() With { _
+                                .ExpressionBindings = { New ExpressionBinding("BeforePrint", "Text", "[QuantityPerUnit]") }, _
+                                .BoundsF = New RectangleF(450,0,100,25), _
+                                .Font = New Font(New FontFamily("Arial"),9) _
+                            }, _
+                            New XRLabel() With { _
+                                .ExpressionBindings = { New ExpressionBinding("BeforePrint", "Text", "[UnitPrice]") }, _
+                                .BoundsF = New RectangleF(550,0,100,25), _
+                                .Font = New Font(New FontFamily("Arial"),9), _
+                                .TextAlignment = TextAlignment.TopRight, _
+                                .TextFormatString = "{0:c2}" _
+                            } _
+                        }, _
+                        .HeightF = 25 _
+                    }, _
+                    New GroupFooterBand() With { _
+                        .Controls = { _
+                            New XRSubreport() With { _
+                                .ReportSource = CreateSubReport(), .GenerateOwnPages = True, .ParameterBindings = { New ParameterBinding("srptCategory",Nothing,"Products.CategoryID") } _
+                            } _
+                        }, _
+                        _
+                        .PageBreak = PageBreak.BeforeBand _
+                    } _
+                }, _
+                .DataSource = CreateDataSource(), _
+                .DataMember = "Products" _
+            }
+            Return report
+        End Function
+        Private Function CreateSubReport() As XtraReport
+            Dim report As New XtraReport() With { _
+                .Bands = { _
+                    New DetailBand() With { _
+                        .Controls = { _
+                            New XRChart() With { _
+                                .BoundsF = New RectangleF(0,0,900,650), .DataMember = "Products", .Series = { _
+                                    New Series() With {.ArgumentDataMember = "ProductName"} _
+                                } _
+                            } _
+                        } _
+                    } _
+                }, _
+                _
+                .Parameters = { _
+                    New Parameter() With { _
+                        .Name = "srptCategory", _
+                        .Type = GetType(System.Int32) _
+                    } _
+                }, _
+                _
+                .Landscape = True, _
+                .DataSource = CreateDataSource() _
+            }
+            Dim chart = TryCast(report.Bands(0).Controls(0), XRChart)
+            chart.Parameters.Add(New XRControlParameter("chartCategory", report.Parameters(0)))
+            chart.Series(0).FilterString = "CategoryID=?chartCategory"
+            chart.Series(0).ValueDataMembers.AddRange(New String() { "UnitPrice"})
+            Return report
+        End Function
+        Public Function CreateDataSource() As Object
+            Dim connectionParameters As New Access97ConnectionParameters(Path.Combine(Path.GetDirectoryName(GetType(Form1).Assembly.Location), "Data/nwind.mdb"), "", "")
+            Dim sqlDataSource As New SqlDataSource(connectionParameters)
+
+            Dim queryProducts As New CustomSqlQuery() With { _
+                .Name = "Products", _
+                .Sql = "SELECT Products.ProductID,Products.ProductName,Products.UnitPrice,Products.QuantityPerUnit,Categories.CategoryID,Categories.CategoryName,Categories.Description,Categories.Picture FROM Products INNER JOIN Categories ON Products.CategoryID=Categories.CategoryID" _
+            }
+            sqlDataSource.Queries.Add(queryProducts)
+            sqlDataSource.Fill()
+
+            Return sqlDataSource
+        End Function
+    End Class
+End Namespace
